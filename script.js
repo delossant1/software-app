e7b971a34c8415781780e2da2ff8c84e54442507
CS 3C Delos Santos Justin <143095707+delossant1@users.noreply.github.com>
2023-12-06 07:35:17 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 scrollToTopBtn.onclick = function () {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
